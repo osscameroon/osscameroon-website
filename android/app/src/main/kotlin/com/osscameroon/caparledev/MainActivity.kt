f4027474c53dc36eeb6de8379df10573e45cdf5f
TCHOUKOUAHA Boris Gautier <btchoukouaha@univ-catho-sjd.com>
2020-11-07 22:06:40 +0100

    Initial App Commit

---
+++ android/app/src/main/kotlin/com/osscameroon/caparledev/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.osscameroon.caparledev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+/*
+ * Created on Fri Nov 06 2020 by Boris Gautier
+ *
+ * Copyright (c) 2020 Osscameroon
+ */
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CaParleDev',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'CaParleDev'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'App Mobile CaParleDev',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
