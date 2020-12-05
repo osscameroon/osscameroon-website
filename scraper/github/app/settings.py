c7152c40c6272ab62e34380db84c3de6d17eb087
elhmn <nleme@live.fr>
2020-12-05 14:29:57 +0100

    feat(scraper/github): get github variables from environ

--- scraper/github/app/settings.py
+++ scraper/github/app/settings.py
@@ -1,11 +1,25 @@
 # settings.py
 # All settings/parameter for the application
 import configparser as cf
+import os
 
 # we read configuration from the config.txt file
 conf = cf.RawConfigParser()
 conf.read(r'config.txt')
 
+def get_github_token():
+    token = conf.get("githubscraper", "GITHUB_TOKEN")
+    if token == "":
+        token = os.environ.get("GITHUB_TOKEN")
+    return token
+
+def get_github_api():
+    api = conf.get("githubscraper", "GITHUB_API")
+    if api == "":
+        api = os.environ.get("GITHUB_API")
+    return api
+
+
 # we get those secrets value
-GITHUB_API = conf.get("githubscraper", "GITHUB_API")
-GITHUB_TOKEN = conf.get("githubscraper", "GITHUB_TOKEN")
+GITHUB_API = get_github_api()
+GITHUB_TOKEN = get_github_token()
