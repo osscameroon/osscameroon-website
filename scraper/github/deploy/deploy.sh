83f6de5c64324ac5c80091f5f030cc0084ce4965
elhmn <nleme@live.fr>
2020-12-05 22:08:37 +0100

    finish deploy

--- scraper/github/deploy/deploy.sh
+++ scraper/github/deploy/deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 FILES=$(cat files.txt)
-TARGET=$(cat target.txt)
+TARGET=$(cat target.txt)/scraper/github
+ARCHIVE=build.tar
 cd ../
 # caparledev-deploy the droplet host (in this case it was set in elhmn's .ssh/config file)
-scp -r $FILES caparledev-deploy:$TARGET
+tar -cvf $ARCHIVE $FILES
+scp -r $ARCHIVE caparledev-deploy:$TARGET
+ssh caparledev-deploy "cd $TARGET && tar -xvf $ARCHIVE && rm -rf build.tar && crontab deploy/cron.txt"
