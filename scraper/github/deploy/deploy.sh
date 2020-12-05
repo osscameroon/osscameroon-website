17dc155ea961cda9d5cbdd104165d60d3bc20d3c
elhmn <nleme@live.fr>
2020-12-05 15:38:40 +0100

    feat(scraper/github): add github scraper action

--- scraper/github/deploy/deploy.sh
+++ scraper/github/deploy/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FILES=$(cat files.txt)
+TARGET=$(cat target.txt)
 cd ../
 # caparledev-deploy the droplet host (in this case it was set in elhmn's .ssh/config file)
-scp -r $FILES caparledev-deploy:/home/deploy/caparledev-website/crons/github
+scp -r $FILES caparledev-deploy:$TARGET
