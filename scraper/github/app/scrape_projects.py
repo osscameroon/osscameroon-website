326e9c2e3dfce92455101578be55debe576bf93d
elhmn <nleme@live.fr>
2020-12-19 12:59:53 +0100

    feat(scraper/github): add github projects scraper

---
+++ scraper/github/app/scrape_projects.py
@@ -0,0 +1,48 @@
+from app.utils.github_requests import get_user_repos, request_failed
+from app.utils.storage import fetch_all_users, store_project
+import time
+
+def filter_relevant_repos(repos):
+    ret = []
+    stargazers_count_threshold = 5
+
+    for r in repos:
+        if r["fork"]:
+            continue
+        if r["stargazers_count"] < stargazers_count_threshold:
+            continue
+        ret.append(r)
+    return ret
+
+def on_pageloaded_success(page):
+    for u in page:
+        user_name = u["login"]
+        print("fetching user {} repositories...".format(user_name))
+        repos = get_user_repos(user_name)
+        if request_failed(repos):
+            print("Error: {}: failed to get user {} repositories.".format(repos, user_name))
+            return
+        print("user {} repositories successfully fetched!".format(user_name))
+        # A relevant repositories is here indentified by criterias
+        # as how many stars the repos has and whether it is a fork or not
+        repos = filter_relevant_repos(repos)
+        repos_len = len(repos)
+        print("relevant: {} repos found.".format(repos_len))
+        if repos_len > 0:
+            print("storing user {} relevant public repositories...".format(user_name))
+            for r in repos:
+                print("storing project {} ...".format(r["name"]))
+                store_project(r)
+                print("project {} stored".format(r["name"]))
+            print("user {} relevant public repositories stored !".format(user_name))
+
+        # we sleep a little to mitigate github api rest limits
+        print("\n")
+    pause_time = 5
+    print("Waiting for {} seconds".format(pause_time))
+    time.sleep(pause_time)
+    print("End page process !\n")
+
+def scrape_projects(prs):
+    print("[+] Getting devs from cameroun/cameroon...")
+    fetch_all_users(on_pageloaded_success=on_pageloaded_success)
---
+++ scraper/github/app/scrape_users.py
@@ -0,0 +1,52 @@
+from app.utils.github_requests import get_users, get_user, request_failed
+from app.utils.storage import store_user
+
+def store_users(users):
+    """
+
+    This callback stores user data in our gcp datastore database
+
+    @params: users array
+
+    """
+
+    print("Storing page users data...")
+    for u in users:
+        if not u:
+            print("Failed to save user data.")
+            continue
+
+        print("Fetch detailed user {}:{} data...".format(u["id"], u["login"]))
+        user = get_user(u["login"])
+        if request_failed(user):
+            print("Failed to save user data: {} .".format(user))
+            return
+
+        print("Storing user {}:{} data...".format(u["id"], u["login"]))
+        store_user(user)
+
+        print("Stored user {}:{} data.".format(u["id"], u["login"]))
+        print("-----------\n")
+    print("Page user data stored.")
+
+
+def on_pageloaded_error(ret):
+    if request_failed(ret):
+        print("Failed to save user data: {} .".format(ret))
+        return
+    print("Failed to save data")
+
+
+def scrape_users(prs):
+    if prs.user_name != "":
+        print("[+] Getting dev information about: {}".format(prs.user_name))
+        user = get_user(prs.user_name)
+        if request_failed(user):
+            print("Failed to save user data: {} .".format(user))
+        else:
+            store_user(user)
+    else:
+        print("[+] Getting devs from cameroun/cameroon...")
+        print("[+] pagination_limit: {}".format(prs.pagination_limit))
+        users = get_users(prs.pagination_limit, store_users, on_pageloaded_error)
+        print(users)
