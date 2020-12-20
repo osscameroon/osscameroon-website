d54035aa919d49a4c10da47cbda91941f78ce83d
sanix-darker <s4nixd@gmail.com>
2020-12-20 13:27:42 +0100

    fix(api): adding flake8

--- api/app/main/utils/decorator.py
+++ api/app/main/utils/decorator.py
@@ -1,22 +1,22 @@
 # from functools import wraps
 #
 # from flask import request
-
+#
 # from app.main.service.auth_helper import Auth
-
-
+#
+#
 # def token_required(f):
 #     @wraps(f)
 #     def decorated(*args, **kwargs):
-
+#
 #         data, status = Auth.get_logged_in_user(request)
 #         token = data.get('data')
-
+#
 #         if not token:
 #             return data, status
-
+#
 #         return f(*args, **kwargs)
-
+#
 #     return decorated
 
 
