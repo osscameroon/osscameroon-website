41051114520cb3cbd9793f9c19d0d71d90d44286
elhmn <nleme@live.fr>
2020-12-22 19:18:26 +0100

    feat(backend): add meilisearch projects index creator

---
+++ tools/meilisearch/app/utils/converters.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+def convert_datetime_fields_to_string(data: dict):
+    """
+    this function converts top level field of type datetime
+    to string
+    """
+
+    for key, val in data.items():
+        if isinstance(val, (datetime.date, datetime.datetime)):
+            data[key] = val.strftime("%Y-%m-%dT%H:%M:%SZ")
+    return data
