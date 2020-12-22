268c1689afa27963702e968f5121ea3e9bb656f2
elhmn <nleme@live.fr>
2020-12-22 21:46:22 +0100

    feat(backend): add projects global search, get projects and get single project endpoint

--- api/app/main/controller/github_controller.py
+++ api/app/main/controller/github_controller.py
@@ -5,21 +5,26 @@ import datetime
 # from app.main.utils.decorator import *
 from app.main.utils.dto import ApiDto
 from app.main.utils.database.users import get_users, get_user
-from app.main.utils.database.search import get_search_users
+from app.main.utils.database.projects import get_projects, get_project
+from app.main.utils.database.search import get_search_users, get_search_projects
 
 api = ApiDto.api
 
 
-# Ex : /users?page=3&count
+# Ex : /users?count=<count>
 @api.route("/users", methods=["GET"])
 class ApidtoUsers(Resource):
     @api.doc("Get_all_users")
     def get(self):
         """This method will return all github users with filter"""
 
-        result = get_users(
-            pagination_limit=request.args.get("page"), count=request.args.get("count")
-        )
+        count = request.args.get("count")
+        if count is not None:
+            count = int(count)
+        else:
+            count = 20
+
+        result = get_users(count=count)
 
         return result, result["code"]
 
@@ -35,7 +40,7 @@ class ApidtoUser(Resource):
         return result, result["code"]
 
 
-# Ex : /search/users?query=<query_string>&count=<element_per_page>&page=<page_number>
+# Ex : /users/search?query=<query_string>&count=<element_per_page>&page=<page_number>
 @api.route("/users/search", methods=["GET"])
 class ApidtoSearch(Resource):
     @api.doc(
@@ -63,4 +68,67 @@ class ApidtoSearch(Resource):
             page = 1
 
         result = get_search_users(query, count, page)
+        return result, result["code"]
+
+
+# Ex : /projects/node-openerp
+@api.route("/projects/<project_name>", methods=["GET"])
+class ApidtoProject(Resource):
+    @api.doc("Get_user_infos")
+    def get(self, project_name):
+        """This request will return a github project by name"""
+
+        result = get_project(project_name)
+        return result, result["code"]
+
+
+# Ex : /projects?count=<count>
+@api.route("/projects", methods=["GET"])
+class ApidtoProjects(Resource):
+    @api.doc(
+        "Get_all_projects",
+        params={
+            "count": "item count",
+        },
+    )
+    def get(self):
+        """This request will return all github projects"""
+
+        count = request.args.get("count")
+        if count is not None:
+            count = int(count)
+        else:
+            count = 20
+
+        page = request.args.get("page")
+        if page is not None:
+            page = int(page)
+        else:
+            page = 1
+
+        result = get_projects(count=count)
+
+        return result, result["code"]
+
+
+# Ex : /projects/search?query=<query_string>&count=<element_per_page>&page=<page_number>
+@api.route("/projects/search", methods=["GET"])
+class ApidtoProjectsSearch(Resource):
+    @api.doc("Get_search_infos")
+    def get(self):
+        query = request.args.get("query")
+
+        count = request.args.get("count")
+        if count is not None:
+            count = int(count)
+        else:
+            count = 20
+
+        page = request.args.get("page")
+        if page is not None:
+            page = int(page)
+        else:
+            page = 1
+
+        result = get_search_projects(query, count, page)
         return result, result["code"]
