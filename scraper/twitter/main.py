75546f00704b81299ac124e1601cbb1f1cd5f2d9
Sherlock Wisdom <wisdomnji@gmail.com>
2020-12-23 15:18:06 +0100

    config file added

--- scraper/twitter/main.py
+++ scraper/twitter/main.py
@@ -5,22 +5,26 @@ from tqdm import tqdm
 import requests
 import mysql.connector
 import json
+import configparser
+
+CONFIGS = configparser.ConfigParser()
+CONFIGS.read( "config.ini" )
 
 # Available from Twitter App
-tweeterBearerToken = ""
+tweeterBearerToken = CONFIGS["AUTH"]["BEARER_TOKEN"]
 headers = {'Authorization': 'Bearer ' + tweeterBearerToken}
 
-'''
 # uncomment if local storage to be tested
 mydb = mysql.connector.connect(
-    host="",
-    user="",
-    password="",
-    database=""
+    host=CONFIGS["MYSQL"]["HOST"],
+    user=CONFIGS["MYSQL"]["USER"],
+    password=CONFIGS["MYSQL"]["PASSWORD"],
+    database=CONFIGS["MYSQL"]["DATABASE"]
 )
+
 # Databse requires a table called tweets with (tweet_id, text) columns
 mydbcursor = mydb.cursor()
-'''
+
 
 def sqlDatabaseInsert( tweet_id, tweet_text ):
 	query = "INSERT INTO tweets (tweet_id, text) VALUES (%s, %s)"
