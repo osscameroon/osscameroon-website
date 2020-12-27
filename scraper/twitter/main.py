148db1d23496465de3f5b06ee43b3d95249868fa
sherlock wisdom <wisdomnji@gmail.com>
2020-12-27 10:01:21 +0100

    should store in GCP now, no idea if it does :p

--- scraper/twitter/main.py
+++ scraper/twitter/main.py
@@ -16,16 +16,17 @@ headers = {'Authorization': 'Bearer ' + tweeterBearerToken}
 # For the gcloud auth to work properly this env variable should be set
 # GOOGLE_APPLICATION_CREDENTIALS=.secrets/service-account.json
 
-# KIND_USERS = "twitter_tweets"
+KIND_PROJECT = "twitter"
 __CLIENT = None
 
 def __get_client():
     global __CLIENT
+    # print( dir(datastore.Client))
     if __CLIENT is None:
         __CLIENT = datastore.Client()
     return __CLIENT
 
-def store_user(tweet_data: dict):
+def store_tweet(tweet_data: dict):
     # Because of not this, defining the type is useless
     if not isinstance(tweet_data, dict):
     	raise TypeError
@@ -36,18 +37,15 @@ def store_user(tweet_data: dict):
     """
 
     client = __get_client()
-    key = client.key("twitter", tweet_data["tweet_id"])
-    # key = client.key(KIND_USERS, user["id"])
+    key = client.key(KIND_PROJECT, tweet_data["id"])
     data = datastore.Entity(key)
-    data.update(user)
+    data.update(tweet_data)
     client.put(data)
 
 
-def fetch_tweets(api_url, parameters):
-    for param in parameters:
-        api_url += f"&{param}={parameters[param]}"
-    print(">> Requesting:", api_url, headers)
-
+def fetch_tweets(api_url, parameters): 
+    for param in parameters: api_url += f"&{param}={parameters[param]}" 
+    print(">> Requesting:", api_url, headers) 
     response = requests.get(api_url, headers=headers)
     results = response.json()
     if response.status_code != 200:
@@ -61,21 +59,19 @@ try:
     # Default query url for api
     api_url = "https://api.twitter.com/2/tweets/search/recent?query=%23caparledev"
     # Configure the api.twitter.com paramaters
-    parameters = {"max_results": 100}
+    parameters = {"max_results": 10}
 
-    datastore = []
     try:
         results = fetch_tweets(api_url, parameters)
-        datastore = results["data"]
         while "next_token" in results["meta"]:
             parameters["next_token"] = results["meta"]["next_token"]
             results = fetch_tweets(api_url, parameters)
-            store_user( results )
-            # datastore = datastore + results["data"]
-            # TODO: do the storing here directly
+            
+            for result in results["data"]:
+                store_tweet( result )
 
     except ValueError as value_error:
         print(">> Error requesting from API:", value_error)
 
-except Exception as e:
-    print(">> runtime error:", repr(e))
+except ValueError as runtime_error:
+    print(">> runtime error:", repr(runtime_error))
