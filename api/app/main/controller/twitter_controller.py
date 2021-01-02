7425ca8dbc95ef53d253df2f176bd73874cd4a70
sanix-darker <s4nixd@gmail.com>
2021-01-03 00:43:15 +0100

    feat(backend): setting controller top-tweets endpoint

---
+++ api/app/main/controller/twitter_controller.py
@@ -0,0 +1,26 @@
+from flask_restplus import Resource, fields
+from flask import request, json
+import datetime
+
+# from app.main.utils.decorator import *
+from app.main.utils.dto import ApiDto
+
+api = ApiDto.api
+
+c = Cache()
+
+# Ex : /top-tweets
+@api.route("/top-tweets", methods=["GET"])
+class ApidtoTopTweets(Resource):
+    @api.doc(
+        "Get_top_tweets"
+    )
+    def get(self):
+        """This method will return all top tweets"""
+
+        result = get_top_tweets(c)
+
+        return result, result["code"]
+
+
+
