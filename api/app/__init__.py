1adb3156850bbe475db32013195c0a524a173b02
elhmn <nleme@live.fr>
2021-01-10 12:56:31 +0000

    feat(api): remove dead code

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,7 +3,6 @@ from flask import Blueprint, url_for
 
 from app.main.controller.github_controller import api as github
 from app.main.controller.twitter_controller import api as twitter
-from app.main.config import app_port
 
 blueprint = Blueprint('api', __name__)
 
