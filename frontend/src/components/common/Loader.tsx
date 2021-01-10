e4a176d825a9b90cdbd2033f6d9b7ba5576fe23e
Eric Cabrel TIOGO <eric@ovrsea.com>
2021-01-10 14:26:00 +0100

    enh(frontend): Translate texts

--- frontend/src/components/common/Loader.tsx
+++ frontend/src/components/common/Loader.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Spinner } from "reactstrap";
+import { useIntl } from "react-intl";
+
+import { commonMessages } from "../../locales/messages";
 
 type LoaderProps = {
   loading: boolean;
 };
 
 const Loader = ({ loading }: LoaderProps) => {
+  const { formatMessage } = useIntl();
+
   if (!loading) return null;
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center p-5">
       <Spinner color="primary" />
-      <div className="mt-2">Loading...</div>
+      <div className="mt-2">{formatMessage(commonMessages.loading)}</div>
     </div>
   );
 };
--- frontend/src/components/common/NetworkError.tsx
+++ frontend/src/components/common/NetworkError.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Alert, Col, Row } from "reactstrap";
+import { useIntl } from "react-intl";
+
+import { commonMessages } from "../../locales/messages";
 
 const NetworkError = () => {
+  const { formatMessage } = useIntl();
+
   return (
     <Row>
       <Col>
         <Alert color="danger">
-          <div className="font-weight-bold">Oops</div>
-          <div>An error occured while processing the request</div>
+          <div className="font-weight-bold">{formatMessage(commonMessages.errorTitle)}</div>
+          <div>{formatMessage(commonMessages.errorMessage)}</div>
         </Alert>
       </Col>
     </Row>
