531ae346e3974b04d09f2dc8a39d9ce849ce6b31
Eric Cabrel TIOGO <eric@ovrsea.com>
2021-01-11 22:42:10 +0100

    enh(frontend): Add title and meta

---
+++ frontend/src/components/appHeader.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Helmet = () => {
+  return (
+    <Helmet>
+      <html lang="en" />
+      <title>React Helmet Tutorial</title>
+      <meta content="Tutorial for React Helmet" name="description" />
+      <meta content="#E6E6FA" name="theme-color" />
+    </Helmet>
+  );
+};
+
+export default Helmet;
