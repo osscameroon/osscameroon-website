c66636645d93896ad3a3b89182f74b703d764efb
elhmn <nleme@live.fr>
2021-01-16 10:25:50 +0100

    chore: remove crontab command from the github scraper deploy.sh

--- scraper/github/deploy/deploy.sh
+++ scraper/github/deploy/deploy.sh
@@ -7,4 +7,4 @@ cd ../
 # caparledev-deploy the droplet host (in this case it was set in elhmn's .ssh/config file)
 tar -cvf $ARCHIVE $FILES
 scp -r $ARCHIVE caparledev-deploy:$TARGET
-ssh caparledev-deploy "cd $TARGET && tar -xvf $ARCHIVE && rm -rf build.tar && crontab deploy/cron.txt"
+ssh caparledev-deploy "cd $TARGET && tar -xvf $ARCHIVE && rm -rf build.tar"
