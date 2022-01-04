4a5a43ad3adba4be9d3edb8289ab6612b4c0d91c
Boris Mbarga <elhmn@users.noreply.github.com>
2022-01-04 09:23:57 +0100

    Change hermes message format (#243)
    * feat(hermes): change hermes message format
    
    * feat: add env variable
    
    * feat: extend to supergroup
    
    * feat: publish on channels

--- bots/hermes/cron_script.py
+++ bots/hermes/cron_script.py
@@ -1,16 +1,24 @@
 import config
 import dbstore
+import os
 from telegram import Bot
 
 ''' Script to be run from a cron job, to fetch recent projects and send to registed 'chats' '''
 
-bot = Bot(token=config.get_config()['Telegram']['token'])
+token = config.get_config()['Telegram']['token']
+if token == "":
+    token = os.getenv('TELEGRAM_TOKEN')
+
+
+telegram_channel_id = os.getenv('TELEGRAM_CHANNEL_ID')
+
+bot = Bot(token=token)
 
 
 def get_project_text(project):
     ''' Returns project as a nicely formatted string '''
 
-    return f"<b>Name</b>: <u>{project['name']}</u>\n\n<b>Description</b>: {project['description']}\n\n<b>Link</b>: {project['html_url']}"
+    return f"Please checkout this Cameroonian open source project\n\n<b>Name</b>: <u>{project['name']}</u>\n\nBy <b>{project['owner']['login']}</b>\n\n<b>Description</b>: {project['description']}\n\n<b>Link</b>: {project['html_url']}"
 
 
 chats = dbstore.get_chats()
@@ -23,4 +31,7 @@ if len(chats) != 0:
             bot.send_message(
                 chat_id=chat['id'], parse_mode='HTML', text=get_project_text(project))
 
+        if telegram_channel_id != "":
+            bot.send_message(
+                chat_id=telegram_channel_id, parse_mode='HTML', text=get_project_text(project))
         dbstore.save_notify(project['id'])
--- bots/hermes/listen_start.py
+++ bots/hermes/listen_start.py
@@ -4,9 +4,14 @@
 import config
 import logging
 import dbstore
+import os
 from telegram.ext import (Updater, CommandHandler)
 
-updater = Updater(token=config.get_config()['Telegram']['token'], use_context=True)
+token = config.get_config()['Telegram']['token']
+if token == "":
+    token = os.getenv('TELEGRAM_TOKEN')
+
+updater = Updater(token=token, use_context=True)
 
 dispatcher = updater.dispatcher
 
@@ -16,10 +21,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 def start(update, context):
     chat = update.message.chat
-
     # get user id, type (group, channel)
-    if chat['type'] == 'group' or chat['type'] == 'channel':
-        dbstore.save_chat({'id': chat['id'], 'type': chat['type']})
+    dbstore.save_chat({'id': chat['id'], 'type': chat['type']})
 
     context.bot.send_message(
         chat_id=update.effective_chat.id, text="Welcome to OSS Cameroon Projects, do well to visit this verious amazing hardwork of our contributors and give them a helping hand!")
