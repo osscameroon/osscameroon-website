ef193763726657c35ff0316109fb28a5d41e6af3
sanix-darker <s4nixd@gmail.com>
2023-04-01 00:47:25 +0200

    test: add error test case for get_env settings

--- scraper/github/tests/test_settings.py
+++ scraper/github/tests/test_settings.py
@@ -1,11 +1,19 @@
 import configparser
+import pytest
+from pytest_mock import MockerFixture
 
 from app.settings import get_key_env
 
 
-def test_get_env(mocker):
-    """ """
+def test_get_env(mocker: MockerFixture) -> None:
     mocker.patch.object(configparser.RawConfigParser, "read")
     mocker.patch.object(configparser.RawConfigParser, "get").return_value = "xxx"
 
-    assert get_key_env("YYY", "ZZZ") == "xxx"
+    assert get_key_env("scope", "something") == "xxx"
+
+    mocker.patch.object(
+        configparser.RawConfigParser, "get"
+    ).side_effect = configparser.NoOptionError("Error while reading", "xxx")
+
+    with pytest.raises(Exception):
+        assert get_key_env("xxx", "something") == "xxx"
